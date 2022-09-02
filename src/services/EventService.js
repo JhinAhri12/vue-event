7d79d8dfe85fdb5a063ad3d63e9d0a4c8f9608d6
JhinAhri12 <cyprien.rezki@hotmail.fr>
2022-09-02 16:02:34 +0200

    add event card and router

---
+++ src/services/EventService.js
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "https://my-json-server.typicode.com/JhinAhri12/EventDB",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+export default {
+  getEvents() {
+    return apiClient.get("/events");
+  },
+};
